1c35deac1a6cfae6af8451c3af22a54a39f48428
nuintun <nuintun@qq.com>
2016-12-06 17:37:41 +0800

    update files

--- layer.js
+++ layer.js
@@ -1,5 +1,5 @@
-import * as Utils from './utils/util';
-import Events from './events/events';
+import * as Utils from '../utils/util';
+import Events from '../events/events';
 import { ZINDEX } from './lib/z-index';
 import { BACKDROP } from './lib/backdrop';
 
