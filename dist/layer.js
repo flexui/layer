e44ff314d343467ca3f420e6fab8a41511b48a3a
nuintun <nuintun@qq.com>
2016-12-07 11:04:24 +0800

    update files

--- dist/layer.js
+++ dist/layer.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('jquery')) :
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('jquery')) :
   typeof define === 'function' && define.amd ? define('layer', ['jquery'], factory) :
-  (factory(global.jQuery));
+  (global.Layer = factory(global.jQuery));
 }(this, (function ($) { 'use strict';
 
   $ = 'default' in $ ? $['default'] : $;
@@ -53,25 +53,38 @@
    * @returns
    */
 
-  // 导出类型判定接口
+  // jquery 对象
+  var win = $(window);
+  var doc = $(document);
+
   /**
    * 属性拷贝
    *
    * @export
    * @param {Object} target 目标对象
    * @param {Object} seed 继承对象
-   * @param {Array} whiteList 白名单
+   * @param {Array} list 名单
+   * @param {Boolean} isWhite 是否是白名单
    */
-  function mix(target, seed, whiteList) {
-    if (!Array.isArray(whiteList)) {
-      whiteList = false;
+  function mix(target, seed, list, isWhite) {
+    if (!Array.isArray(list)) {
+      list = false;
     }
+
+    var index;
 
     // Copy "all" properties including inherited ones.
     for (var prop in seed) {
       if (seed.hasOwnProperty(prop)) {
         // 检测白名单
-        if (whiteList && whiteList.indexOf(prop) === -1) continue;
+        if (list) {
+          index = list.indexOf(prop);
+
+          // 区分黑白名单
+          if (isWhite ? index === -1 : index !== -1) {
+            continue;
+          }
+        }
 
         // 在 iPhone 1 代等设备的 Safari 中，prototype 也会被枚举出来，需排除
         if (prop !== 'prototype') {
@@ -309,15 +322,211 @@
     }
   };
 
+  /**
+   * Layer
+   *
+   * @constructor
+   * @export
+   */
   function Layer() {
+    var context = this;
 
+    context.destroyed = false;
+    context.node = document.createElement('div');
+    context.__node = $(context.node)
+      .attr('tabindex', '-1')
+      .css({
+        display: 'none',
+        position: 'absolute',
+        outline: 0
+      });
   }
 
-  Layer.ZINDEX = ZINDEX;
-  Layer.BACKDROP = BACKDROP;
+  // 当前得到焦点的实例
+  Layer.active = null;
+  // 层级
+  Layer.zIndex = ZINDEX;
+  // 锁屏遮罩
+  Layer.backdrop = BACKDROP.node;
 
-  inherits(Layer, Events, {
+  // 锁定 tab 焦点在弹窗内
+  doc.on('focusin', function(e) {
+    var active = Layer.active;
 
+    if (active && active.modal) {
+      var target = e.target;
+      var node = active.node;
+
+      if (target !== node && !node.contains(target)) {
+        active.focus();
+      }
+    }
   });
+
+  // 原型方法
+  inherits(Layer, Events, {
+    /**
+     * 浮层 DOM 元素节点
+     * @public
+     * @readonly
+     */
+    node: null,
+    /**
+     * 判断对话框是否删除
+     * @public
+     * @readonly
+     */
+    destroyed: true,
+    /**
+     * 判断对话框是否显示
+     * @public
+     * @readonly
+     */
+    open: false,
+    /**
+     * 是否自动聚焦
+     * @public
+     * @property
+     */
+    autofocus: true,
+    /**
+     * 是否是模态窗口
+     * @public
+     * @property
+     */
+    modal: false,
+    /**
+     * 内部的 HTML 字符串
+     * @public
+     * @property
+     */
+    innerHTML: '',
+    /**
+     * CSS 类名
+     * @public
+     * @property
+     */
+    className: 'ui-layer',
+    /**
+     * 构造函数
+     * @public
+     * @readonly
+     */
+    constructor: Layer,
+    /**
+     * 让浮层获取焦点
+     * @public
+     */
+    focus: function() {
+      var context = this;
+
+      // 销毁，未打开和已经得到焦点不做处理
+      if (context.destroyed || !context.open) {
+        return context;
+      }
+
+      var node = context.node;
+      var layer = context.__node;
+      var active = Layer.active;
+
+      if (active && active !== context) {
+        active.blur(false);
+      }
+
+      // 检查焦点是否在浮层里面
+      if (!layer.contains(context.__getActive())) {
+        var autofocus = layer.find('[autofocus]')[0];
+
+        if (!context.__autofocus && autofocus) {
+          context.__autofocus = true;
+        } else {
+          autofocus = node;
+        }
+
+        // 获取焦点
+        context.__focus(autofocus);
+      }
+
+      // 非激活状态才做处理
+      if (active !== context) {
+        var index = context.zIndex = Layer.zIndex++;
+
+        // 设置遮罩层级
+        Backdrop.zIndex(index);
+        // 设置弹窗层级
+        layer.css('zIndex', index);
+        // 添加激活类名
+        layer.addClass(context.className + '-focus');
+        // 触发事件
+        context.emit('focus');
+
+        // 保存当前激活实例
+        Layer.active = context;
+      }
+
+      return context;
+    },
+    /**
+     * 让浮层失去焦点。将焦点退还给之前的元素，照顾视力障碍用户
+     * @public
+     */
+    blur: function() {
+      var context = this;
+
+      // 销毁和未打开不做处理
+      if (context.destroyed || !context.open) {
+        return context;
+      }
+
+      var isBlur = arguments[0];
+      var activeElement = context.__activeElement;
+
+      if (isBlur !== false) {
+        context.__focus(activeElement);
+      }
+
+      context.__autofocus = false;
+
+      context.__node.removeClass(context.className + '-focus');
+      context.emit('blur');
+
+      return context;
+    },
+    /**
+     * 对元素安全聚焦
+     * @private
+     * @param {HTMLElement} element
+     */
+    __focus: function(element) {
+      // 防止 iframe 跨域无权限报错
+      // 防止 IE 不可见元素报错
+      try {
+        // ie11 bug: iframe 页面点击会跳到顶部
+        if (this.autofocus && !/^iframe$/i.test(element.nodeName)) {
+          element.focus();
+        }
+      } catch (e) {
+        // error
+      }
+    },
+    /**
+     * 获取当前焦点的元素
+     * @private
+     */
+    __getActive: function() {
+      try {
+        // try: ie8~9, iframe #26
+        var activeElement = document.activeElement;
+        var contentDocument = activeElement.contentDocument;
+        var element = contentDocument && contentDocument.activeElement || activeElement;
+
+        return element;
+      } catch (e) {
+        // error
+      }
+    }
+  });
+
+  return Layer;
 
 })));
--- layer.js
+++ layer.js
@@ -1,15 +1,210 @@
+import $ from 'jquery';
 import Events from '@flexui/events';
 import * as Utils from '@flexui/utils';
 import { ZINDEX } from './lib/z-index';
 import { BACKDROP } from './lib/backdrop';
 
-function Layer() {
+/**
+ * Layer
+ *
+ * @constructor
+ * @export
+ */
+export default function Layer() {
+  var context = this;
 
+  context.destroyed = false;
+  context.node = document.createElement('div');
+  context.__node = $(context.node)
+    .attr('tabindex', '-1')
+    .css({
+      display: 'none',
+      position: 'absolute',
+      outline: 0
+    });
 }
 
-Layer.ZINDEX = ZINDEX;
-Layer.BACKDROP = BACKDROP;
+// 当前得到焦点的实例
+Layer.active = null;
+// 层级
+Layer.zIndex = ZINDEX;
+// 锁屏遮罩
+Layer.backdrop = BACKDROP.node;
 
+// 锁定 tab 焦点在弹窗内
+Utils.doc.on('focusin', function(e) {
+  var active = Layer.active;
+
+  if (active && active.modal) {
+    var target = e.target;
+    var node = active.node;
+
+    if (target !== node && !node.contains(target)) {
+      active.focus();
+    }
+  }
+});
+
+// 原型方法
 Utils.inherits(Layer, Events, {
+  /**
+   * 浮层 DOM 元素节点
+   * @public
+   * @readonly
+   */
+  node: null,
+  /**
+   * 判断对话框是否删除
+   * @public
+   * @readonly
+   */
+  destroyed: true,
+  /**
+   * 判断对话框是否显示
+   * @public
+   * @readonly
+   */
+  open: false,
+  /**
+   * 是否自动聚焦
+   * @public
+   * @property
+   */
+  autofocus: true,
+  /**
+   * 是否是模态窗口
+   * @public
+   * @property
+   */
+  modal: false,
+  /**
+   * 内部的 HTML 字符串
+   * @public
+   * @property
+   */
+  innerHTML: '',
+  /**
+   * CSS 类名
+   * @public
+   * @property
+   */
+  className: 'ui-layer',
+  /**
+   * 构造函数
+   * @public
+   * @readonly
+   */
+  constructor: Layer,
+  /**
+   * 让浮层获取焦点
+   * @public
+   */
+  focus: function() {
+    var context = this;
 
+    // 销毁，未打开和已经得到焦点不做处理
+    if (context.destroyed || !context.open) {
+      return context;
+    }
+
+    var node = context.node;
+    var layer = context.__node;
+    var active = Layer.active;
+
+    if (active && active !== context) {
+      active.blur(false);
+    }
+
+    // 检查焦点是否在浮层里面
+    if (!layer.contains(context.__getActive())) {
+      var autofocus = layer.find('[autofocus]')[0];
+
+      if (!context.__autofocus && autofocus) {
+        context.__autofocus = true;
+      } else {
+        autofocus = node;
+      }
+
+      // 获取焦点
+      context.__focus(autofocus);
+    }
+
+    // 非激活状态才做处理
+    if (active !== context) {
+      var index = context.zIndex = Layer.zIndex++;
+
+      // 设置遮罩层级
+      Backdrop.zIndex(index);
+      // 设置弹窗层级
+      layer.css('zIndex', index);
+      // 添加激活类名
+      layer.addClass(context.className + '-focus');
+      // 触发事件
+      context.emit('focus');
+
+      // 保存当前激活实例
+      Layer.active = context;
+    }
+
+    return context;
+  },
+  /**
+   * 让浮层失去焦点。将焦点退还给之前的元素，照顾视力障碍用户
+   * @public
+   */
+  blur: function() {
+    var context = this;
+
+    // 销毁和未打开不做处理
+    if (context.destroyed || !context.open) {
+      return context;
+    }
+
+    var isBlur = arguments[0];
+    var activeElement = context.__activeElement;
+
+    if (isBlur !== false) {
+      context.__focus(activeElement);
+    }
+
+    context.__autofocus = false;
+
+    context.__node.removeClass(context.className + '-focus');
+    context.emit('blur');
+
+    return context;
+  },
+  /**
+   * 对元素安全聚焦
+   * @private
+   * @param {HTMLElement} element
+   */
+  __focus: function(element) {
+    // 防止 iframe 跨域无权限报错
+    // 防止 IE 不可见元素报错
+    try {
+      // ie11 bug: iframe 页面点击会跳到顶部
+      if (this.autofocus && !/^iframe$/i.test(element.nodeName)) {
+        element.focus();
+      }
+    } catch (e) {
+      // error
+    }
+  },
+  /**
+   * 获取当前焦点的元素
+   * @private
+   */
+  __getActive: function() {
+    try {
+      // try: ie8~9, iframe #26
+      var activeElement = document.activeElement;
+      var contentDocument = activeElement.contentDocument;
+      var element = contentDocument && contentDocument.activeElement || activeElement;
+
+      return element;
+    } catch (e) {
+      // error
+    }
+  }
 });
